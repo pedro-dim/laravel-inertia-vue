8c2d30175fc8585cc1e43b686a8712c4c60e1537
Your Name <pcontato19@gmail.com>
2022-09-24 15:40:29 +0100

    Create Models and Migrations

---
+++ app/Models/Project.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+
+
+    protected $fillable=[
+    'skill_id',
+    'project',
+    'name',
+    'image',
+    'project_url'
+
+    ];
+
+
+public function getSkill(){
+
+    return $this->belongsTo(Skill::class);
+
+
+}
+
+}
---
+++ app/Models/Skill.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Skill extends Model
+{
+    use HasFactory;
+
+
+
+    protected $fillable=[
+
+        'name',
+        'image',
+
+
+        ];
+
+
+    public function getProjects(){
+
+        return $this->hasMany(Project::class);
+
+
+    }
+}
